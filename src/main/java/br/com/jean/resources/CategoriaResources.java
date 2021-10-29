e95b79218bc9a89bae462a8fca22fa01fb362649
jeanpereira3 <jeansilv13@gmail.com>
2021-10-29 16:24:05 -0300

    Teste primeira classe de dominio

--- src/main/java/br/com/jean/resources/CategoriaResources.java
+++ src/main/java/br/com/jean/resources/CategoriaResources.java
@@ -1,16 +1,28 @@
 package br.com.jean.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.jean.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResources {
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "REST esta funcionando";
+	public List<Categoria> listar() {
+		Categoria cat1 = new Categoria(1, "informatica");
+		Categoria cat2 = new Categoria(2, "Escritorio");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 	
 }
