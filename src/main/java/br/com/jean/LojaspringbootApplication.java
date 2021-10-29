8054f4a955025cdf23b5752f17c710c504e5ab7b
jeanpereira3 <jeansilv13@gmail.com>
2021-10-29 15:22:53 -0300

    Projeto criado

---
+++ src/main/java/br/com/jean/LojaspringbootApplication.java
@@ -0,0 +1,13 @@
+package br.com.jean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaspringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/jean/LojaspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaspringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
