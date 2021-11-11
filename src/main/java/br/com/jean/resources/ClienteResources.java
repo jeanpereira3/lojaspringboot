f3691cd80ec526087f80cf4c6a20c5d962f57f60
jeanpereira3 <jeansilv13@gmail.com>
2021-11-11 13:41:57 -0300

    Inserindo um novo Cliente com POST

--- src/main/java/br/com/jean/resources/ClienteResources.java
+++ src/main/java/br/com/jean/resources/ClienteResources.java
@@ -1,5 +1,6 @@
 package br.com.jean.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,9 +15,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import br.com.jean.domain.Categoria;
 import br.com.jean.domain.Cliente;
 import br.com.jean.dto.ClienteDTO;
+import br.com.jean.dto.ClienteNewDTO;
 import br.com.jean.services.ClienteService;
 
 @RestController
@@ -31,6 +35,15 @@ public class ClienteResources {
 		
 		Cliente obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
+		
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO objDto) {
+		Cliente obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 		
 	}
 	
