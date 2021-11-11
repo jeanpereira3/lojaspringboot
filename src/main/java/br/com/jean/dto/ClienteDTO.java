c1ca0bb6d95f115ed274718108549af9a6c54f2c
jeanpereira3 <jeansilv13@gmail.com>
2021-11-11 15:10:14 -0300

    Validacao para email nao repetido na atualizacao de cliente

--- src/main/java/br/com/jean/dto/ClienteDTO.java
+++ src/main/java/br/com/jean/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import br.com.jean.domain.Cliente;
+import br.com.jean.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
