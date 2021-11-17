bf5e6fa273e4b325c5b1216f02635db246627080
jeanpereira3 <jeansilv13@gmail.com>
2021-11-17 14:51:16 -0300

    Salvando primeiro arquivo no s3

--- src/main/java/br/com/jean/LojaspringbootApplication.java
+++ src/main/java/br/com/jean/LojaspringbootApplication.java
@@ -1,11 +1,17 @@
 package br.com.jean;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.jean.services.S3Service;
+
 @SpringBootApplication
 public class LojaspringbootApplication implements CommandLineRunner{
+	
+	@Autowired
+	private S3Service s3Service;
 
 	public static void main(String[] args) {
 		SpringApplication.run(LojaspringbootApplication.class, args);
@@ -13,7 +19,7 @@ public class LojaspringbootApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
-			
+		//s3Service.uploadFile("D:\\projetos\\java udemy\\1cc481c0-d28a-4f01-814f-1d9c0d3d2a26.jpeg");
 	}
 
 }
