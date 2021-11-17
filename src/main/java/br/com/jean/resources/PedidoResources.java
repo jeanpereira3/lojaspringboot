59fd97a756a8ec4eda147010266f3063a384ad42
jeanpereira3 <jeansilv13@gmail.com>
2021-11-17 10:21:20 -0300

    Refresh token

--- src/main/java/br/com/jean/resources/PedidoResources.java
+++ src/main/java/br/com/jean/resources/PedidoResources.java
@@ -15,9 +15,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import br.com.jean.domain.Categoria;
 import br.com.jean.domain.Pedido;
-import br.com.jean.dto.CategoriaDTO;
 import br.com.jean.services.PedidoService;
 
 @RestController
